e0598f1a073c93b2abbe3f8a35544ab3e442d681
Monteth <greezlie@gmail.com>
2019-02-13 22:28:15 +0100

    Search form update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,40 +1,87 @@
 import React, {Component} from "react"
-import {NavLink} from "react-router-dom"
+import {Link, NavLink, Redirect, Route} from "react-router-dom"
 
 class Header extends Component {
 
-    redirect() {
+    state = {
+        searchRedirect: false,
+        searchString: ""
+    }
 
+    searchForSoftware(event) {
+        const {name, value} = event.target
+        this.setState({
+            searchRedirect: true
+        })
+        console.log("request redirect")
+    }
+
+    handleChange = (event) => {
+        const {name, value} = event.target
+        this.setState({
+            searchString: value,
+            searchRedirect: true
+        })
+        console.log("save searchString")
+    }
+
+    submitForm() {
+
+        // this.props.history.push({pathname: '/apps', state: {searchString: this.state.searchString}})
+        return <Link to={{
+            pathname: '/apps',
+            searchString: this.state.searchString
+        }}/>
     }
 
     render() {
         return (
-            <header className="navi">" "
-                <div className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <div>
 
-                    <NavLink className="navbar-brand" to="/">SpokoSklep</NavLink>
+                {/*{this.state.searchRedirect ? <Link to={{pathname: '/apps', state: this.state.searchString}}/> : null}*/}
+                <header className="navi">" "
+                    <div className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"/>
-                    </button>
-                    <div className="collapse navbar-collapse" id="nabvarCollapse">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
+                        <NavLink className="navbar-brand" to="/">SpokoSklep</NavLink>
+
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"/>
+                        </button>
+                        <div className="collapse navbar-collapse" id="nabvarCollapse">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
                                     <NavLink className="nav-link" to="/apps">Aplikacje</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/account">Konto</NavLink>
-                            </li>
-                        </ul>
-                        <form className="form-inline mt-2 mt-md-0">
-                            <input className="form-control mr-sm-2" type="text" placeholder="Wyszukaj..."/>
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Szuk</button>
-                        </form>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/account">Konto</NavLink>
+                                </li>
+                            </ul>
+                            <form className="form-inline"  onSubmit={this.submitForm}>
+                                <input
+                                    onKeyDown={(event) => (event.keyCode === 13) ? this.form.submit() : null }
+                                    size="25"
+                                    name="name"
+                                    className="form-control mr-sm-2"
+                                    type="text"
+                                    onChange={this.handleChange}
+                                    placeholder="..."/>
+                                    <button type="submit">Szuk</button>
+                                <Link className="btn btn-outline-success my-2 my-sm-0"
+                                      to={
+                                          {
+                                              pathname: '/apps',
+                                              searchString: this.state.searchString
+                                          }
+                                      }>
+                                    Szukaj
+                                </Link>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </header>
+                </header>
+            </div>
         )
     }
 }
